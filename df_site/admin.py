9980a5d551404e41210865e1cba8fe0b08e9cad9
d9pouces <github@19pouces.net>
2024-09-06 21:47:04 +0200

    feat: a complete testcase

--- df_site/admin.py
+++ df_site/admin.py
@@ -1,10 +1,8 @@
 """Admin classes for the df_site app."""
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext as _
 
-from df_site.models import AlertRibbon, PreferencesUser
+from df_site.models import AlertRibbon
 
 
 @admin.register(AlertRibbon)
@@ -15,27 +13,3 @@ class AlertRibbonAdmin(admin.ModelAdmin):
     list_filter = ("color", "start_date", "end_date", "is_active", "position")
     search_fields = ("summary",)
     fields = ["summary", "url", "message", "color", "start_date", "end_date", "is_active", "position"]
-
-
-@admin.register(PreferencesUser)
-class PreferencesUserAdmin(UserAdmin):
-    """Admin class for the custom user model."""
-
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    ("is_active", "is_staff"),
-                    "is_superuser",
-                    ("email_notifications", "display_online"),
-                    "color_theme",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
---
+++ test_paint_manager/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the paint_manager module."""
