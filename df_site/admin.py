b56ca196937142801f3df5b9f606a4472f9872c5
d9pouces <github@19pouces.net>
2024-10-24 14:13:18 +0200

    fix: import error when django-allauth is not used

--- df_site/admin.py
+++ df_site/admin.py
@@ -1,23 +1,5 @@
 """Admin classes for the df_site app."""
 
-from functools import cached_property
-
-try:
-    from allauth.account.models import EmailAddress
-except ImportError:
-    EmailAddress = None
-try:
-    from allauth.mfa.models import Authenticator
-except ImportError:
-    Authenticator = None
-try:
-    from allauth.socialaccount.models import SocialAccount
-except ImportError:
-    SocialAccount = None
-try:
-    from allauth.usersessions.models import UserSession
-except ImportError:
-    UserSession = None
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
@@ -36,58 +18,75 @@ class AlertRibbonAdmin(admin.ModelAdmin):
     fields = ["summary", "url", "message", "color", "start_date", "end_date", "is_active", "position"]
 
 
-class EmailAddressInline(admin.TabularInline):
-    """Inline for the email address model."""
+if "allauth.account" in settings.INSTALLED_APPS:
+    from allauth.account.models import EmailAddress
 
-    model = EmailAddress
-    extra = 0
+    class EmailAddressInline(admin.TabularInline):
+        """Inline for the email address model."""
 
+        model = EmailAddress
+        extra = 0
+else:
+    EmailAddressInline = None
 
-class SocialAccountInline(admin.TabularInline):
-    """Inline for the social account model."""
+if "allauth.mfa" in settings.INSTALLED_APPS:
+    from allauth.mfa.models import Authenticator
 
-    model = SocialAccount
-    extra = 0
-    fields = ["provider", "last_login", "date_joined"]
-    readonly_fields = ["provider", "last_login", "date_joined"]
+    class AuthenticatorInline(admin.TabularInline):
+        """Inline for the authenticator model."""
 
-    def has_add_permission(self, request, obj):
-        """Return False to prevent adding new social accounts."""
-        return False
+        model = Authenticator
+        extra = 0
+        fields = ["type", "created_at", "last_used_at"]
+        readonly_fields = ["type", "created_at", "last_used_at"]
 
+        def has_add_permission(self, request, obj):
+            """Return False to prevent adding new authenticators."""
+            return False
+else:
+    AuthenticatorInline = None
 
-class AuthenticatorInline(admin.TabularInline):
-    """Inline for the authenticator model."""
+if "allauth.socialaccount" in settings.INSTALLED_APPS:
+    from allauth.socialaccount.models import SocialAccount
 
-    model = Authenticator
-    extra = 0
-    fields = ["type", "created_at", "last_used_at"]
-    readonly_fields = ["type", "created_at", "last_used_at"]
+    class SocialAccountInline(admin.TabularInline):
+        """Inline for the social account model."""
 
-    def has_add_permission(self, request, obj):
-        """Return False to prevent adding new authenticators."""
-        return False
+        model = SocialAccount
+        extra = 0
+        fields = ["provider", "last_login", "date_joined"]
+        readonly_fields = ["provider", "last_login", "date_joined"]
 
+        def has_add_permission(self, request_, obj_):
+            """Return False to prevent adding new social accounts."""
+            return False
+else:
+    SocialAccountInline = None
 
-class UserSessionInline(admin.TabularInline):
-    """Inline for the user session model."""
+if "allauth.usersessions" in settings.INSTALLED_APPS:
+    from allauth.usersessions.models import UserSession
 
-    model = UserSession
-    extra = 0
-    fields = [
-        "created_at",
-        "ip",
-        "last_seen_at",
-    ]
-    readonly_fields = [
-        "created_at",
-        "ip",
-        "last_seen_at",
-    ]
+    class UserSessionInline(admin.TabularInline):
+        """Inline for the user session model."""
 
-    def has_add_permission(self, request, obj):
-        """Return False to prevent adding new user sessions."""
-        return False
+        model = UserSession
+        extra = 0
+        fields = [
+            "created_at",
+            "ip",
+            "last_seen_at",
+        ]
+        readonly_fields = [
+            "created_at",
+            "ip",
+            "last_seen_at",
+        ]
+
+        def has_add_permission(self, request_, obj):
+            """Return False to prevent adding new user sessions."""
+            return False
+else:
+    UserSessionInline = None
 
 
 @admin.register(PreferencesUser)
@@ -119,24 +118,10 @@ class PreferencesUserAdmin(UserAdmin):
         (_("Important dates"), {"fields": (("last_login", "date_joined"),)}),
     )
 
-    @cached_property
-    def excluded_inlines(self):
-        """Exclude inlines of which the corresponding app is not installed."""
-        excluded = []
-        if "allauth.mfa" not in settings.INSTALLED_APPS or Authenticator is None:
-            excluded.append(AuthenticatorInline)
-        if "allauth.socialaccount" not in settings.INSTALLED_APPS or SocialAccount is None:
-            excluded.append(SocialAccountInline)
-        if "allauth.usersessions" not in settings.INSTALLED_APPS or UserSession is None:
-            excluded.append(UserSessionInline)
-        if "allauth.account" not in settings.INSTALLED_APPS or EmailAddress is None:
-            excluded.append(EmailAddressInline)
-        return excluded
-
     def get_inlines(self, request, obj):
         """Return inlines excluding those that correspond to apps not installed."""
         inlines = super().get_inlines(request, obj)
         if obj is None:
             return []
-        inlines = [x for x in inlines if x not in self.excluded_inlines]
+        inlines = [x for x in inlines if x is not None]
         return inlines
