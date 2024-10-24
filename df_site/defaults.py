519289d30af9709cfae3f91f17e141a78990551f
d9pouces <github@19pouces.net>
2024-10-24 16:50:36 +0200

    fix: correct use of middlewares

--- df_site/defaults.py
+++ df_site/defaults.py
@@ -96,31 +96,9 @@ DF_INSTALLED_APPS = [
 ]
 
 DF_MIDDLEWARE = [
-    "allauth.usersessions.middleware.UserSessionsMiddleware",
-    "df_websockets.middleware.WebsocketMiddleware",
     "df_site.middleware.websocket_middleware",
 ]
-MIDDLEWARE = [
-    "django_prometheus.middleware.PrometheusBeforeMiddleware",
-    "django.middleware.cache.UpdateCacheMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "df_config.apps.middleware.DFConfigMiddleware",
-    "allauth.usersessions.middleware.UserSessionsMiddleware",
-    "df_websockets.middleware.WebsocketMiddleware",
-    "df_site.middleware.websocket_middleware",
-    "csp.middleware.CSPMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
-    "django.middleware.cache.FetchFromCacheMiddleware",
-    "django_prometheus.middleware.PrometheusAfterMiddleware",
-]
+
 USERSESSIONS_TRACK_ACTIVITY = True
 POSTMAN_DISALLOW_ANONYMOUS = True
 POSTMAN_AUTO_MODERATE_AS = True
